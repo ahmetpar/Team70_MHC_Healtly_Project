3ee31953d818a75acfc76544293636cc0d3bceda
SenturkCelik <senturkcelik.lt@gmail.com>
2022-08-26 12:39:24 +0300

    senturk2

---
+++ src/test/java/pojos/pojo_US_28/Country1Request.java
@@ -0,0 +1,13 @@
+package pojos.pojo_US_28;
+
+import com.github.javafaker.Faker;
+
+public class Country1Request {
+
+    public static void main(String[] args) {
+
+     String name ="Barcelona";
+
+
+    }
+}
